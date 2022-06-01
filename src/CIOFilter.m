389eb3d8057b3b0babdc2dbfe5bb5e4c927a5303
rugwing <aac.wong@gmail.com>
2022-05-31 23:01:54 -0700

    filter

---
+++ src/CIOFilter.m
@@ -0,0 +1,85 @@
+classdef CIOFilter < handle
+    % specific filter for the CIO localization problem
+    %   particle filter that maintains pose
+    %   array of ekfs that are each associated with a particle filter
+    %   force estimate which is passed as an additional measurement to each filter
+    properties 
+        N;
+        % struct of known robot parameters
+        params;
+        % a normal struct
+        pf_system;
+        % instantiation of class
+        ekf_system;
+        % particle filter of pose
+        pf;
+        % ekfs associated with each particle 
+        ekfs;
+        F_hat_hist;
+        M_hat_hist;
+
+    end
+    methods
+        function obj = CIOFilter(pf_system,ekf_system)
+            obj.pf_system = pf_system;
+            obj.ekf_system = ekf_system;
+            obj.pf = ParticleFilter(system);
+        end
+        % initializes with prior particles of robot pose and mean+sigma of bias states  and drift rates
+        % particles are struct particles.X, particles.W
+        function [] = initialize(obj,particles,mu,sigma)
+            obj.pf.initialize_particles(particles);
+            obj.N = numel(particles.W);
+            obj.ekfs(1:obj.N) = EKF(obj.ekf_system);
+            obj.ekfs.initialize(mu,sigma);
+        end
+
+        function [] = predict(obj,u_t)
+            % should really use a transition model prediction instead
+            obj.pf.predict(u_t);
+            obj.ekfs.predict(u_t);
+            % give ekfs the list of particle states 
+            % which they should all share supposedly
+            obj.ekfs(1).system.params.X = obj.pf.particles.X;
+        end
+
+        % replaces the particle filter update step
+        function [] = update(obj,u_t,y_t)
+            f_b = y_t(1:2);
+            w_b = y_t(3);
+            % calculate estimated F and M
+            K_F = obj.params.K_F;
+            K_M = obj.params.K_M;
+            m = obj.params.m;
+            I_z = obj.params.I_z;
+            dt = obj.params.dt;
+            F_hat_prev = obj.F_hat_hist(:,end);
+            M_hat_prev = obj.M_hat_hist(:,end);
+            F = K_F * (m*f_b - F_in - F_hat_prev)*dt;
+            M = K_M * (I_z*w_b -(M_in + M_hat_prev)*dt);
+
+            % augment with pseudomeasurement
+            y_t_aug = [y_t;F;M];
+
+            % updating ekfs need a way to index ekfs
+            nums = num2cell([1:obj.N]);
+            [obj.ekfs.num] = deal(nums{:});
+            obj.ekfs.update(u_t,y_t_aug);
+            
+            % particle filter update a-la fast slam
+            W_bar = prod(reshape([obj.particles.ekf.p_yj],[],obj.N));
+            W_hat = W_bar.*obj.particles.W;
+            W_t = W_hat/sum(W_hat);
+            obj.particles.W = W_t;
+            % need to create a resample condition
+            %resample = std(W_t)>0.03/sqrt(obj.N);
+            resample = true;
+            if resample
+                [idx_resample] = obj.pf.importance_resample();
+                obj.ekfs = obj.ekfs(idx_resample);
+            end
+
+            obj.F_hat_hist(:,end+1) = F;
+            obj.M_hat_hist(:,end+1) = M;
+        end
+    end
